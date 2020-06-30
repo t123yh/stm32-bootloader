6ca097a7b97882e51783eb32447a09860f2b40c7
Yunhao Tian <t123yh@outlook.com>
2020-06-30 17:16:42 +0800

    My bootloader.

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,13 @@
-#include "api.h"
-#include "api-asm.h"
+#include <libopencm3/cm3/scb.h>
+
+#define APP_ADDRESS	0x08008000
 
 int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+    /* Set vector table base address. */
+    SCB_VTOR = APP_ADDRESS & 0xFFFF;
+    /* Initialise master stack pointer. */
+    asm volatile("msr msp, %0"::"g"
+        (*(volatile uint32_t *)APP_ADDRESS));
+    /* Jump to application. */
+    (*(void (**)())(APP_ADDRESS + 4))();
 }
